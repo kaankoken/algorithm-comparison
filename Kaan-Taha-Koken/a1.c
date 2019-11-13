61f7f76f02df749bb46a7595b5dce9fa23378fc9
kaan.koken <kaan_taha_koken@hotmail.com>
2019-11-13 13:25:51 +0200

    Inıtial file commit

---
+++ Kaan-Taha-Koken/a1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+    Available on github after deadline
+    <a href ="https://github.com/kaankoken/algorithm-comparison">Github</a>
+    Kaan Taha Köken - 2152064
+*/
+
+int main(int argc, char **argv) {
+    
+    return 0;
+}
